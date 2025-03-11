0438c6f87ec4545606b060266a81d132e4d3c4d8
egorb <egorbobylev398@gmail.com>
2025-03-12 03:04:51 +0500

    upd

--- src/routes/metricsRoutes/index.ts
+++ src/routes/metricsRoutes/index.ts
@@ -5,62 +5,86 @@ import { and, between, eq, isNotNull, ne, sql } from "drizzle-orm";
 import { snapshotsTable, tokensTable } from "../../db/schema";
 import dayjs from "dayjs";
 
-const metricsRoutes = new Elysia({ prefix: "/metrics" }).get(
-  "/topByOperations",
-  async ({ query: { firstDate, secondDate }, set }) => {
-    try {
-      const from = dayjs(firstDate);
-      const to = dayjs(secondDate);
-      const query = await db
-        .selectDistinctOn([snapshotsTable.currencyName], {
-          currencyName: snapshotsTable.currencyName,
-          created: snapshotsTable.created,
-          contract: snapshotsTable.contract,
-          countOps: snapshotsTable.countOps,
-          valueChange: sql<number>`
+const metricsRoutes = new Elysia({ prefix: "/metrics" })
+  .get(
+    "/topByOperations",
+    async ({ query: { firstDate, secondDate }, set }) => {
+      try {
+        const from = dayjs(firstDate);
+        const to = dayjs(secondDate);
+        const query = await db
+          .selectDistinctOn([snapshotsTable.currencyName], {
+            currencyName: snapshotsTable.currencyName,
+            created: snapshotsTable.created,
+            contract: snapshotsTable.contract,
+            countOps: snapshotsTable.countOps,
+            valueChange: sql<number>`
           CASE
             WHEN LAG(${snapshotsTable.countOps}) OVER (PARTITION BY ${snapshotsTable.contract} ORDER BY created) IS NULL THEN NULL
             ELSE (${snapshotsTable.countOps}::NUMERIC - LAG(${snapshotsTable.countOps}) OVER (PARTITION BY ${snapshotsTable.contract} ORDER BY created))
           END
         `,
-        })
-        .from(snapshotsTable)
-        .where(
-          and(
-            isNotNull(snapshotsTable.countOps),
-            between(snapshotsTable.created, from.toDate(), to.toDate())
-          )
-        );
+          })
+          .from(snapshotsTable)
+          .where(
+            and(
+              isNotNull(snapshotsTable.countOps),
+              between(snapshotsTable.created, from.toDate(), to.toDate())
+            )
+          );
 
-      if (query) {
-        for (let res of query) {
-          if (res.countOps) {
-            Object.assign(res, {
-              percentageChange: (
-                (res.valueChange / res.countOps) *
-                100
-              ).toPrecision(6),
-            });
-          } else {
-            Object.assign(res, {
-              percentageChange: null,
-            });
+        if (query) {
+          for (let res of query) {
+            if (res.countOps) {
+              Object.assign(res, {
+                percentageChange: (
+                  (res.valueChange / res.countOps) *
+                  100
+                ).toPrecision(6),
+              });
+            } else {
+              Object.assign(res, {
+                percentageChange: null,
+              });
+            }
           }
+          set.status = 200;
+          return query;
         }
-        set.status = 200;
-        return query;
+      } catch (error) {
+        set.status = 500;
+        return error;
       }
-    } catch (error) {
-      set.status = 500;
-      return error;
+    },
+    {
+      query: t.Object({
+        firstDate: t.String(),
+        secondDate: t.String(),
+      }),
     }
-  },
-  {
-    query: t.Object({
-      firstDate: t.String(),
-      secondDate: t.String(),
-    }),
-  }
-);
+  )
+  .get(
+    "tokenMetrics",
+    async ({ query: { tokenAddress }, set }) => {
+      try {
+        const snapshots = await db
+          .select()
+          .from(snapshotsTable)
+          .where(eq(snapshotsTable.contract, tokenAddress));
+        if (snapshots.length === 0) {
+          throw new Error("Данного адреса нет в базе данных");
+        }
+        return snapshots;
+      } catch (Error) {
+        set.status = 500;
+        return (Error as Error).message;
+      }
+    },
+    {
+      query: t.Object({
+        tokenAddress: t.String(),
+      }),
+    }
+  );
 
 export default metricsRoutes;
